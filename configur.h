f11d0354caa7da887da52f7baae8a47b39e62e3d
Abhishek Parmar <abhi.pa.9301@gmail.com>
2026-01-10 22:52:49 +0530

    Create configur.h

---
+++ configur.h
@@ -0,0 +1,12 @@
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+#define MQ_PIN A0
+
+#define MOTOR_IN1 8
+#define MOTOR_IN2 9
+#define MOTOR_IN3 10
+#define MOTOR_IN4 11
+
+#define BUZZER 6
+#define LED 7
